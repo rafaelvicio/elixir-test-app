196a383b4defb47da8621ac7c7350e3d7dc99d46
Rafael Vicio <rafaelvicio@icloud.com>
2018-05-05 13:38:10 -0300

    Config test db"

--- config/test.exs
+++ config/test.exs
@@ -8,11 +8,3 @@ config :elixir_test_app, ElixirTestApp.Endpoint,
 
 # Print only warnings and errors during test
 config :logger, level: :warn
-
-# Configure your database
-config :elixir_test_app, ElixirTestApp.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  url: [scheme: "https", host: "https://rafaelvicio-elixir.herokuapp.com/", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-  ssl: true
